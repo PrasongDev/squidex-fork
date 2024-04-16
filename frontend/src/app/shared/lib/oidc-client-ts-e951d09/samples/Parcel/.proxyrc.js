896a7ad5ae07f49237a43be8e95d7c111a250a61
PrasongDev <Prasong-prasong@hotmail.com>
2024-04-16 22:25:39 +0700

    add app/shared/lib/oidc-client-ts-e951d09

---
+++ frontend/src/app/shared/lib/oidc-client-ts-e951d09/samples/Parcel/.proxyrc.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware("/oidc/", {
+            target: "http://localhost:15000/",
+        })
+    );
+
+    app.use('/code-flow-duendesoftware', (req, res, next) => {
+        res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
+        res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
+        next();
+    });
+};
